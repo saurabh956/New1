2334a50058bc0daaf1ecddb3f7d7f921ffb74ed5
nitin-git-code <nitin.javastudent@gmail.com>
2019-08-20 13:27:59 +0530

    Lambda expression has been added inside java project.

--- GitPro/src/Demo.java
+++ GitPro/src/Demo.java
@@ -1,11 +1,10 @@
 import pack1.MyInterface;
-import pack1.SomeClass;
 
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MyInterface ref=SomeClass.getMyInterface();
+		MyInterface ref=()->{System.out.println("inside lambda disp");};
 		ref.disp();
 	}
 
